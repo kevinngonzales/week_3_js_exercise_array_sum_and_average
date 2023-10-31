903c9c0c738d7942bc19ac7d5c9a969850678d86
Kevinn Gonzales <147567696+kevinngonzales@users.noreply.github.com>
2023-10-30 23:01:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var numbers = [2,6,10,15,19];
+var numbers = [2,6,10,15,19]; // Create an array of numbers with at least 5 elements.
 
-function calculateAverage(numbers) {
+function calculateAverage(numbers) { //Write a loop that iterates over the array and calculates the sum of all the elements.
   let sum = 0
   for (let i=0; i < numbers.length; i++) {
     sum += numbers[i];
   }
-  let avg = sum / numbers.length;
+  let avg = sum / numbers.length; //Calculate the average of the array elements by dividing the sum by the total number of elements.
  return avg
 }
 
-console.log (calculateAverage(numbers))
+console.log (calculateAverage(numbers)) //Display the sum and average using console.log
